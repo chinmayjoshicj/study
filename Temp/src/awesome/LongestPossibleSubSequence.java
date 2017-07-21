0b31ba0831e0d3c55ee5bd141610a0f01b3156c9
Chinmay Joshi <1993chinmay.joshi@gmail.com>
2017-07-21 16:06:02 +0530

    ss

--- Temp/src/awesome/LongestPossibleSubSequence.java
+++ Temp/src/awesome/LongestPossibleSubSequence.java
@@ -5,14 +5,14 @@ public class LongestPossibleSubSequence {
 	public static void main(String[] args) {
 		String a = "Traveller";
 		String b = "rellevarT";
-		/*
-		 * String a="abcdaf"; String b="acbcf";
-		 */
+		
+		 /*String a="abcdaf"; String b="acbcf";*/
+		 
 
-		int mat[][] = new int[a.length()][b.length()];
+		int mat[][] = new int[b.length()][a.length()];
 
-		char s1[] = a.toCharArray();
-		char s2[] = b.toCharArray();
+		char s1[] = b.toCharArray();
+		char s2[] = a.toCharArray();
 
 		for (int i = 0; i < s1.length; i++) {
 			for (int j = 0; j < s2.length; j++) {
@@ -20,11 +20,10 @@ public class LongestPossibleSubSequence {
 					if (i == 0 && j == 0) {
 						mat[i][j] = 1;
 					} else if (i == 0 || j == 0) {
-						if (i == 0) {
-							mat[i][j] = mat[i][j - 1] + 1;
-						} else {
-							mat[i][j] = mat[i - 1][j] + 1;
-						}
+						
+						
+							mat[i][j] = mat[i][j] + 1;
+						
 					} else {
 						mat[i][j] = mat[i - 1][j - 1] + 1;
 					}
@@ -42,7 +41,6 @@ public class LongestPossibleSubSequence {
 				}
 			}
 		}
-
 		for (int i = 0; i < s1.length; i++) {
 			for (int j = 0; j < s2.length; j++) {
 
@@ -50,6 +48,19 @@ public class LongestPossibleSubSequence {
 			}
 			System.out.println();
 		}
+		
+		/*System.out.println();
+		for (int i = s1.length; i < 0; i--) 
+		{
+			for (int j = s2.length; j <0 ; j--) 
+			{
+				if (mat[i-1][j-1]) {
+					
+				}
+			}
+			System.out.println();
+		}
+*/
 
 	}
 }
