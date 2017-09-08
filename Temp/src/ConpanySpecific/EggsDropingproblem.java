630f7e564f4225632062ca612f5a5c01ef7a1616
Chinmay Joshi <1993chinmay.joshi@gmail.com>
2017-09-08 18:45:36 +0530

    nextegg

--- Temp/src/ConpanySpecific/EggsDropingproblem.java
+++ Temp/src/ConpanySpecific/EggsDropingproblem.java
@@ -1,12 +1,9 @@
 package ConpanySpecific;
 
-import java.awt.Checkbox;
 import java.util.Scanner;
 
 public class EggsDropingproblem {
 
-	static int criticalfloor=Integer.MAX_VALUE;
-	static int count=1;
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
@@ -21,22 +18,24 @@ public class EggsDropingproblem {
 			int n = Integer.parseInt(a[0]);
 			
 			int k = Integer.parseInt(a[1]);
-			
 			int criticalFloor=findCriticalFloor(n,k);
-			System.out.println(Math.min(criticalFloor,check(n,k)));
+			System.out.println(check(n,criticalFloor,k, criticalFloor,0));
 				
 		}
 	}
 
-	private static int check(int n, int k) 
+	private static int check(int n, int cFloor, int k, int originalCriticalFloor,int count) 
 	{
-			
-		//if it doesn't break
-		if (n<1 || k<1) 
+		int i=0;
+		i++;
+		if (k<cFloor)
 		{
-			return 0;
+			return count;
 		}
-		return 1+Math.min(findCriticalFloor(n,k),check(n-1, k-1));
+		System.out.println("CFloor "+((count+1)*originalCriticalFloor-count));
+		int first=cFloor-(count*(originalCriticalFloor-1));
+		count=1+Math.min((count+1)*originalCriticalFloor-count,check(n, cFloor+originalCriticalFloor-1,k, originalCriticalFloor,count+1));
+		return count;
 	}
 
 	private static int findCriticalFloor(int n, int k) 
@@ -44,8 +43,8 @@ public class EggsDropingproblem {
 		int root1=(int)Math.round(((-1)+Math.sqrt((1*1)-(4*(1)*(-2)*(k))))/2*(1));
 		int root2=(int)Math.round(((-1)-Math.sqrt((1*1)-(4*(1)*(-2)*(k))))/2*(1));
 		
-		System.out.println("root1 "+root1);
-		System.out.println("root2 "+root2);
+		/*System.out.println("root1 "+root1);
+		System.out.println("root2 "+root2);*/
 		
 		return (root1 > root2)? root1: root2;
 	}
