140ba56778d58da0b36737076249b848a68c1e5e
Chinmay Joshi <1993chinmay.joshi@gmail.com>
2017-09-21 12:39:43 +0530

    thread

--- Temp/src/awesome/RemovingDuplicates.java
+++ Temp/src/awesome/RemovingDuplicates.java
@@ -1,35 +1,91 @@
 package awesome;
 
+import java.util.Scanner;
+
 public class RemovingDuplicates 
 {
-	static String a="Pradeed";
-	public static void main(String[] args) 
-	{
-		RemoveDup(a);
-		System.out.println(a);
-	}
-
-	private static String RemoveDup(String a) 
-	{
-		char b[]=a.toCharArray();
-		for (int i = 0; i < a.length()-1; i++)
-		{
-			if (b[i]==b[i+1])
-			{
-				System.out.println(a.charAt(i)+" "+a.charAt(i+1));
-				System.out.println(b[i]+" "+b[i+1]);
-				swap(i,i+1);
+	 /*static String s="baab";
+	    public static void main(String[] args)
+	    {
+	        char a[]=s.toCharArray();
+	        removeDuplicates(a,0);
+	        if (s.isEmpty())
+	        {
+	        	System.out.println("Empty String");
 			}
-		}
-		return b.toString();
-	}
-	private static void swap(int i, int j) 
-	{
-		for (int j2 = j; j2 <= a.length(); j2++) 
-		{
-			System.out.println(a.charAt(i)+" "+a.charAt(j2));
-			a.replace(a.charAt(i), a.charAt(j2));
-			i++;
-		}
-	}
+	        else
+	        {
+	        	System.out.println(s);
+	        }
+	    }
+
+	    private static void removeDuplicates(char[] a, int start) 
+	    {
+	        int i=0;
+	        if (start>=a.length-1)
+	        {
+	            s=String.valueOf(a);
+	            return;
+	        }
+	        for (i = 0; i < a.length-1; i++)
+	        {
+	            if (a[i]==a[i+1])
+	            {
+	                int k=i;
+	                for (int j = i+2; j < a.length; j++) 
+	                {
+	                    a[k++]=a[j];
+	                }
+	                i--;
+	                String s=String.valueOf(a);
+	                a=s.substring(0, a.length-2).toCharArray();
+	                break;
+	            }
+	        }
+	        removeDuplicates(a, i);
+	    }*/
+	static String ss=null;
+    static String super_reduced_string(String s)
+    {
+        ss=s;
+            char a[]=ss.toCharArray();
+	        removeDuplicates(a,0);
+	        if (ss.isEmpty())
+	        {
+	        	System.out.println("Empty String");
+	        }
+			return ss;
+    }
+     private static void removeDuplicates(char[] a, int start) 
+	    {
+	        int i=0;
+	        if (start>=a.length-1)
+	        {
+	            ss=String.valueOf(a);
+	            return;
+	        }
+	        for (i = 0; i < a.length-1; i++)
+	        {
+	            if (a[i]==a[i+1])
+	            {
+	                int k=i;
+	                for (int j = i+2; j < a.length; j++) 
+	                {
+	                    a[k++]=a[j];
+	                }
+	                i--;
+	                String s=String.valueOf(a);
+	                a=s.substring(0, a.length-2).toCharArray();
+	                break;
+	            }
+	        }
+	        removeDuplicates(a, i);
+	    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String result = super_reduced_string(s);
+        System.out.println(result);
+    }
 }
