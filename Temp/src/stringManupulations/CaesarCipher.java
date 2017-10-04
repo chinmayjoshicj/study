9b47bd1cd07fcce29fd6b6349858c5b39ca24c49
Chinmay Joshi <1993chinmay.joshi@gmail.com>
2017-10-04 17:37:14 +0530

    CaesarCipher

--- Temp/src/stringManupulations/CaesarCipher.java
+++ Temp/src/stringManupulations/CaesarCipher.java
@@ -4,23 +4,31 @@ package stringManupulations;
 Caesar's cipher rotated every letter in a string by a fixed number, , making it unreadable by his enemies.
 Given a string, , and a number, , encrypt  and print the resulting string.*/
 
-public class CaesarCipher 
-{
-	public static void main(String[] args) 
-	{
-		String str="Middle-Outz";
-		StringBuffer b= new StringBuffer(str);
-		
-		String finalString=new String(str);
-		int k=2;
-		
-		for (int i = 0; i < str.length(); i++)
-		{
-			if (str.charAt(i)>=65 && str.charAt(i)<=90) 
-			{
-				b.replace(i, i,((int)str.charAt(i)+k)+"");
+public class CaesarCipher {
+	public static void main(String[] args) {
+		String s = "middle-Outz";
+		StringBuffer finalString = new StringBuffer(s);
+		int k = 2;
+		k=k%26;
+		char temp = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (finalString.charAt(i) >= 65 && finalString.charAt(i) <= 90) {
+				if ((finalString.charAt(i) + k) > 90) {
+					temp = (char) ((finalString.charAt(i) + k) % 90 + 64);
+				} else
+					temp = (char) (finalString.charAt(i) + k);
+
+				finalString = finalString.replace(i, i + 1, (temp) + "");
+			} else if (finalString.charAt(i) >= 97 && finalString.charAt(i) <= 122) {
+				if ((finalString.charAt(i) + k) > 122) {
+					temp = (char) ((finalString.charAt(i) + k) % 122 + 96);
+				} else {
+					temp = (char) (finalString.charAt(i) + k);
+				}
+				finalString = finalString.replace(i, i + 1, (temp) + "");
 			}
 		}
-		System.out.println(b.toString());
+		System.out.println(finalString);
 	}
+
 }
