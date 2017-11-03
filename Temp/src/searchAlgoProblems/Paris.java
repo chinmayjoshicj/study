c89a9751b5007eb2e645a73f986235ec20b29d28
Chinmay Joshi <1993chinmay.joshi@gmail.com>
2017-11-03 18:12:19 +0530

    parisIncomplete

--- Temp/src/searchAlgoProblems/Paris.java
+++ Temp/src/searchAlgoProblems/Paris.java
@@ -1,5 +1,7 @@
 package searchAlgoProblems;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /*
@@ -9,6 +11,8 @@ Input Format
 
 The first line contains N and K. 
 The second line contains N numbers of the set. All the N numbers are unique.*/
+
+/*Incomplete*/
 public class Paris
 {
 	public static void main(String[] args) 
@@ -17,19 +21,38 @@ public class Paris
 		int n = in.nextInt();
 		int k=in.nextInt();
 		int a[]=new int[n];
+		ArrayList<Integer>a1=new ArrayList<>();
 		for (int i = 0; i<n; i++)
 		{
 			a[i]=in.nextInt();
 		}
 		int count=0;
+		int j=0;
 		for (int i = 0; i < a.length; i++) 
 		{
-			for (int j = i; j < a.length; j++) 
+			/*for (int j = i; j < a.length; j++) 
 			{
 				if (Math.abs(a[i]-a[j])==k) 
 				{
 					count++;
 				}
+			}*/
+			a1.add(a[i]);
+		}
+		ArrayList<Integer>a2=new ArrayList<>();
+		a2.addAll(a1);
+		HashMap<Integer, Integer> map=new HashMap<>();
+		for (Integer integer : a1) 
+		{
+				map.put(integer+k, 1);
+				map.put(integer-k, 1);
+		}
+		for (Integer integer : a1) 
+		{
+			if (map.containsKey(integer))
+			{
+				map.remove(integer);
+				count++;
 			}
 		}
 		System.out.println(count);
