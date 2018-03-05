89fe3f0011dc464b30dd7fa06b5975f52216d988
Chinmay Joshi <1993chinmay.joshi@gmail.com>
2018-03-05 14:12:08 +0530

    MandatoryText

--- Temp/src/CompanyUtility/SearchTextMandatory.java
+++ Temp/src/CompanyUtility/SearchTextMandatory.java
@@ -1,5 +1,9 @@
 package CompanyUtility;
+
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -18,57 +22,93 @@ import org.xml.sax.SAXParseException;
 public class SearchTextMandatory {
 
 	private static final XPath XPATH = XPathFactory.newInstance().newXPath();
-		public static void main(String[] args) throws Exception 
-		{	
-			try {
-				DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-			    DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-			    Document doc = docBuilder.parse (new File("C:\\Users\\Dell\\Desktop\\rad.xml.txt"));
 
-			    // normalize text representation
-			    doc.getDocumentElement().normalize();
-			    System.out.println ("Root element of the doc is " + doc.getDocumentElement().getNodeName());
+	public static void main(String[] args) throws Exception {
+		
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+		 List<String> readAllLines = Files.readAllLines(Paths.get("D:\\Sources\\12.5.0.0.0_core_supp_installer\\SOURCE\\FCUBS_14.0.0.0.0\\test.txt"));
+//		 List<String> readAllLines = Files.readAllLines(Paths.get("C:\\Users\\cmjoshi\\Desktop\\path.txt"));
 
-			    NodeList listOfBooks = doc.getElementsByTagName("book");
-			    int totalBooks = listOfBooks.getLength();
-			    System.out.println("Total no of books : " + totalBooks);
+		for (String fileName : readAllLines) {
+			int count = 0;
+			
+		try {
+			Document doc = docBuilder.parse(new File(fileName));
 
-			    for(int i=0; i<listOfBooks.getLength() ; i++) {
+			// normalize text representation
+			doc.getDocumentElement().normalize();
+//			System.out.println("Root element of the doc is " + doc.getDocumentElement().getChildNodes().item(1).getNodeValue().trim());
+			
+			NodeList rootChildren = doc.getDocumentElement().getChildNodes();
+			for (int i = 0; i < rootChildren.getLength(); i++) {
+				Node item = rootChildren.item(i);
+				
+				if(item.getNodeType() == Node.ELEMENT_NODE &&item.getNodeName().  equals("FUNCTION_ID")) {
+//					functionId =  item.getNodeValue();
+					System.out.print(item.getFirstChild().getTextContent()+"\t");
+				}
+			}
 
-			        Node firstBookNode = listOfBooks.item(i);
- 			        if(firstBookNode.getNodeType() == Node.ELEMENT_NODE) {
+			NodeList blockFields = doc.getElementsByTagName("RAD_BLK_FIELDS");
+			int totaldataBlocks = blockFields.getLength();
+			//System.out.println("Total no of Blocks : " + totaldataBlocks);
 
-			            Element firstElement = (Element)firstBookNode;                              
-			            System.out.println("Year :"+firstElement.getAttribute("year"));
+			for (int i = 0; i < blockFields.getLength(); i++) {
 
-			            //-------
-			            NodeList firstNameList = firstElement.getElementsByTagName("title");
-			            Element firstNameElement = (Element)firstNameList.item(0);
+				boolean required = false;
+				boolean textField = false;
+				Node firstDataBlock = blockFields.item(i);
+				if (firstDataBlock.getNodeType() == Node.ELEMENT_NODE) {
 
-			            NodeList textFNList = firstNameElement.getChildNodes();
-			            System.out.println("title : " + ((Node)textFNList.item(0)).getNodeValue().trim());
-			        }
-			    }//end of for loop with s var
-			} catch (SAXParseException err) {
-			    System.out.println ("** Parsing error" + ", line " + err.getLineNumber () + ", uri " + err.getSystemId ());
-			    System.out.println(" " + err.getMessage ());
-			} catch (SAXException e) {
-			    Exception x = e.getException ();
-			    ((x == null) ? e : x).printStackTrace ();
-			} catch (Throwable t) {
-			    t.printStackTrace ();
-			}                
+					Element firstElement = (Element) firstDataBlock;
+
+					NodeList childNodes = firstElement.getChildNodes();
+					for (int j = 0; j < childNodes.getLength(); j++) {
+						Node item = childNodes.item(j);
+
+						if (item.getNodeType() == Node.ELEMENT_NODE && item.getNodeName().equals("REQUIRED")
+								&& item.getFirstChild() != null
+								&& item.getFirstChild().getTextContent().trim().equals("Y")) {
+							required = true;
+
+						}
+						if (item.getNodeType() == Node.ELEMENT_NODE && item.getNodeName().equals("DISPLAY_TYPE")
+								&& item.getFirstChild() != null
+								&& item.getFirstChild().getTextContent().trim().equals("TEXT")) {
+							textField = true;
+
+						}
+					}
+				}
+
+				if (required && textField) {
+					/*System.out.println("REQUIRED & Text for : "
+							+ firstDataBlock.getAttributes().getNamedItem("ID").getTextContent());*/
+					count++;
+				}
+
+			} // end of for loop with s var
+			System.out.print(count);
+			System.out.println();
+		} catch (SAXParseException err) {
+			System.out.println("** Parsing error" + ", line " + err.getLineNumber() + ", uri " + err.getSystemId());
+			System.out.println(" " + err.getMessage());
+		} catch (SAXException e) {
+			Exception x = e.getException();
+			((x == null) ? e : x).printStackTrace();
+		} catch (Throwable t) {
+			t.printStackTrace();
 		}
-
-		public static NodeList selectNodeList(Node contextNode, String xpathStr) throws XPathExpressionException 
-		{
-			return (NodeList) XPATH.evaluate(xpathStr, contextNode, XPathConstants.NODESET);
 		}
+	}
 
-		public static Node selectSingleNode(Node contextNode, String xpathStr) throws XPathExpressionException 
-		{
-			return (Node) XPATH.evaluate(xpathStr, contextNode, XPathConstants.NODE);
-		}
+	public static NodeList selectNodeList(Node contextNode, String xpathStr) throws XPathExpressionException {
+		return (NodeList) XPATH.evaluate(xpathStr, contextNode, XPathConstants.NODESET);
+	}
+
+	public static Node selectSingleNode(Node contextNode, String xpathStr) throws XPathExpressionException {
+		return (Node) XPATH.evaluate(xpathStr, contextNode, XPathConstants.NODE);
+	}
 
 }
-
